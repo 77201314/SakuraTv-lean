6ca8c1f7fd346f6d432b044c043d5fcdafb29d22
ShenLan2108 <36469469+ShenLan2108@users.noreply.github.com>
2020-02-24 13:17:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,23 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo '修改机器名称'
+sed -i 's/OpenWrt/SakuraTv/g' package/base-files/files/bin/config_generate
+
+echo '下载主题'
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/rosywrt/luci-theme-rosy package/luci-theme-rosy
+git clone https://github.com/Lienol/openwrt-package package/Lienol
+
+echo '修改网关地址'
+sed -i 's/192.168.1.1/192.168.1.17/g' package/base-files/files/bin/config_generate
+
+echo '修改默认主题'
+sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+echo '去除默认bootstrap主题'
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+echo '修改banner'
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
