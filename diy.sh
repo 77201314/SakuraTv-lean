1430c6528d322182980d8b1e718f3cb7a4083817
ShenLan2108 <36469469+ShenLan2108@users.noreply.github.com>
2020-02-26 22:41:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,6 +13,7 @@ echo "rm -f /tmp/luci-indexcache" >>package/lean/default-settings/files/zzz-defa
 echo "exit 0" >>package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/SakuraTv/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.1.16/g' package/base-files/files/bin/config_generate
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
 
