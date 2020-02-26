243ff12e4363c7dcee0cadea0df1ce81be336fca
ShenLan2108 <36469469+ShenLan2108@users.noreply.github.com>
2020-02-26 16:54:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,9 +11,6 @@ sed -i 's/OpenWrt/SakuraTv/g' package/base-files/files/bin/config_generate
 echo '修改网关地址'
 sed -i 's/192.168.1.1/192.168.1.16/g' package/base-files/files/bin/config_generate
 
-echo '去除默认bootstrap主题'
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
