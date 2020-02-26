f1246b5a023d8e49e064d591fc5758696814b519
ShenLan2108 <36469469+ShenLan2108@users.noreply.github.com>
2020-02-26 17:59:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,16 +1,76 @@
-#!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
-echo '修改机器名称'
+sed -i 's/openwrt.proxy.ustclug.org/op.hyird.xyz/g' package/lean/default-settings/files/zzz-default-settings
+version=$(cat ../version)
+sed -i '33 d' package/lean/default-settings/files/zzz-default-settings
+sed -i '33 d' package/lean/default-settings/files/zzz-default-settings
+sed -i '44 d' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/exit 0//g' package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's/snapshots/$version/g'  /etc/opkg/distfeeds.conf " >>package/lean/default-settings/files/zzz-default-settings
+echo "echo \"DISTRIB_REVISION='$version'\" >> /etc/openwrt_release" >>package/lean/default-settings/files/zzz-default-settings
+sed -i '52 d' package/lean/default-settings/files/zzz-default-settings
+sed -i '52 d' package/lean/default-settings/files/zzz-default-settings
+echo "rm -rf /tmp/luci-modulecache/" >>package/lean/default-settings/files/zzz-default-settings
+echo "rm -f /tmp/luci-indexcache" >>package/lean/default-settings/files/zzz-default-settings
+echo "exit 0" >>package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/SakuraTv/g' package/base-files/files/bin/config_generate
-
-echo '修改网关地址'
 sed -i 's/192.168.1.1/192.168.1.16/g' package/base-files/files/bin/config_generate
-
-echo '修改banner'
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
+
+#add sonme additional packages
+git clone https://github.com/rufengsuixing/luci-app-adguardhome         package/adg
+git clone https://github.com/destan19/OpenAppFilter                     package/oaf
+git clone https://github.com/Advanced-noob/luci-theme-atmaterial.git    package/atmaterial
+git clone https://github.com/vernesong/OpenClash                        package/clash
+
+cat << EOF >> target/linux/ipq40xx/config-4.14
+CONFIG_PMBUS=n
+CONFIG_SENSORS_PMBUS=n
+CONFIG_SENSORS_ADM1275=n
+CONFIG_SENSORS_IBM_CFFPS=n
+CONFIG_SENSORS_IR35221=n
+CONFIG_SENSORS_LM25066=n
+CONFIG_SENSORS_LTC2978=n
+CONFIG_SENSORS_LTC3815=n
+CONFIG_SENSORS_MAX16064=n
+CONFIG_SENSORS_MAX20751=n
+CONFIG_SENSORS_MAX34440=n
+CONFIG_SENSORS_MAX8688=n
+CONFIG_SENSORS_TPS40422=n
+CONFIG_SENSORS_TPS53679=n
+CONFIG_SENSORS_UCD9000=n
+CONFIG_SENSORS_UCD9000=n
+CONFIG_SENSORS_UCD9200=n
+CONFIG_NETWORK_FILESYSTEMS=y
+CONFIG_NFS_FS=y
+CONFIG_CIFS_ALLOW_INSECURE_LEGACY=y
+CONFIG_CIFS_WEAK_PW_HASH=n
+EOF
+
+cat << EOF >> target/linux/x86/config-4.19
+CONFIG_HSA_AMD=n
+EOF
+
+cat << EOF >> target/linux/ramips/mt7621/config-4.14
+CONFIG_PMBUS=n
+CONFIG_SENSORS_PMBUS=n
+CONFIG_SENSORS_ADM1275=n
+CONFIG_SENSORS_IBM_CFFPS=n
+CONFIG_SENSORS_IR35221=n
+CONFIG_SENSORS_LM25066=n
+CONFIG_SENSORS_LTC2978=n
+CONFIG_SENSORS_LTC3815=n
+CONFIG_SENSORS_MAX16064=n
+CONFIG_SENSORS_MAX20751=n
+CONFIG_SENSORS_MAX34440=n
+CONFIG_SENSORS_MAX8688=n
+CONFIG_SENSORS_TPS40422=n
+CONFIG_SENSORS_TPS53679=n
+CONFIG_SENSORS_UCD9000=n
+CONFIG_SENSORS_UCD9000=n
+CONFIG_SENSORS_UCD9200=n
+CONFIG_NETWORK_FILESYSTEMS=y
+CONFIG_NFS_FS=y
+CONFIG_CIFS_ALLOW_INSECURE_LEGACY=y
+CONFIG_CIFS_WEAK_PW_HASH=n
+CONFIG_DMA_JZ4780=n
+EOF
