f901d0f3dbc125b67dd994843144ada2677f719b
77201314 <36469469+77201314@users.noreply.github.com>
2020-08-07 13:26:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,8 +6,8 @@
 # Blog: https://p3terx.com
 #=================================================
 #修改版本号
-sed -i 's/OpenWrt/SakuraTv $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/OpenWrt/SakuraTv/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/334420 $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/334420/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 rm -rf package/base-files/files/etc/banner
